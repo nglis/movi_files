bac04119f287fc4972850e26d6a41259caabaf2a
nglis <nglisic10@gmail.com>
2020-12-28 23:41:54 -0700

    Gutted create react app project

---
+++ movi/src/Containers/HomePage.js
@@ -0,0 +1,13 @@
+function HomePage() {
+    return(
+        <div className="App">
+        <header className="App-header">
+          <p>
+            Start
+          </p>
+        </header>
+      </div>
+    );
+}
+
+export default HomePage;
