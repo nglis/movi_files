161d9bd11214abdd5376eca8267600d22d3834ab
nglis <nglisic10@gmail.com>
2020-12-30 23:51:40 -0700

    Temporary home page

--- movi/src/Containers/HomePage.js
+++ movi/src/Containers/HomePage.js
@@ -1,11 +1,23 @@
+import HeroDisplay from '../Components/HeroDisplay';
+
 function HomePage() {
+    const details = { 
+      title: "TRAVELERS",
+      year: "2018",
+      rating: "TV-MA",
+      length: "3 Seasons",
+      episode: 'S3:E2 "Yates"',
+      description: "MacLaren and Yates adjust to their new partnership while protecting a TV show host whose inflammatory rhetoric about Travelers has led to tragedy"
+    };
+  
     return(
         <div className="App">
-        <header className="App-header">
-          <p>
-            Start
-          </p>
-        </header>
+          <header>
+              This is where the app bar will go
+          </header>
+          <HeroDisplay 
+            details={details}
+          />
       </div>
     );
 }
