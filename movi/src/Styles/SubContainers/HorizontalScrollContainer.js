5896cd8c7fa22cf164be0eb45badc202337a04af
nglis <nglisic10@gmail.com>
2021-01-01 22:14:06 -0700

    Scroll container changes

--- movi/src/Styles/SubContainers/HorizontalScrollContainer.js
+++ movi/src/Styles/SubContainers/HorizontalScrollContainer.js
@@ -1,23 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      flexWrap: 'nowrap',
-      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-      transform: 'translateZ(0)',
-    },
-    title: {
-      color: theme.palette.primary.light,
-    },
-    titleBar: {
-      background:
-        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-  }));
+  root: {
+    margin: '30px',
+    overflow: 'hidden',
+  },
+  scrollBox: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'flex-start'
+  },
+  imgDimensions: {
+    height: '233px',
+    width: '166px',
+    marginRight: '8px'
+  },
+  text: {
+    marginLeft: '30px',
+    textAlign: 'left'
+  }
+}));
--- movi/src/SubContainers/HorizontalScrollContainer.js
+++ movi/src/SubContainers/HorizontalScrollContainer.js
@@ -1,57 +1,85 @@
-import React from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import tileData from './tileData';
+import React, { useState, useEffect, useRef } from 'react';
 
+import img from '../mission_impossible_poster.jpg';
 import { useStyles } from '../Styles/SubContainers/HorizontalScrollContainer';
 
+const tileData = [
+    // {
+    //     img: img,
+    //     title: 'TEST',
+    //     author: 'test'
+    // },
+];
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
+function HorizontalScrollContainer(props) {
+    const { collectionTitle } = props;
 
-function HorizontalScrollContainer() {
+    const [scrollDivScrollWidth, setScrollDivScrollWidth] = useState(0);
+    const [scrollDivClientWidth, setScrollDivClientWidth] = useState(0);
+
+    const [dragging, setDragging] = useState(true);
+    const [scrollAmount, setScrollAmount] = useState(0);
+    const [mousePosition, setMousePosition] = useState( { x: -1, y: -1} );
+
+    const scrollDivRef = useRef();
     const classes = useStyles();
 
+    const handleMouseDown = e => {
+        setDragging(true);
+        setMousePosition( { x: e.clientX, y: e.clientY } );
+
+        e.stopPropagation();
+        e.preventDefault();
+    } 
+
+    const handleMouseMove = e => {
+        if (!dragging) return;
+
+        const shift = scrollAmount + mousePosition.x - e.clientX;
+
+        if (shift > scrollDivScrollWidth - (scrollDivClientWidth)) {
+            return;
+        }
+
+        setScrollAmount(shift < 0 ? 0 : shift);
+        setMousePosition( { x: e.clientX, y: e.clientY } );
+    }
+
+    const handleEndDrag = () => {
+        if (!dragging) return;
+
+        setDragging(false);
+    }
+
+    useEffect(() => {
+        setScrollDivScrollWidth(scrollDivRef.current.scrollWidth);
+        setScrollDivClientWidth(scrollDivRef.current.clientWidth);
+    }, []);
+
     return(
-        <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5}>
-            {tileData.map((tile) => (
-                <GridListTile key={tile.img}>
-                <img src={tile.img} alt={tile.title} />
-                <GridListTileBar
-                    title={tile.title}
-                    classes={{
-                    root: classes.titleBar,
-                    title: classes.title,
-                    }}
-                    actionIcon={
-                    <IconButton aria-label={`star ${tile.title}`}>
-                        <StarBorderIcon className={classes.title} />
-                    </IconButton>
-                    }
-                />
-                </GridListTile>
-            ))}
-            </GridList>
-        </div>
+        <>
+            <div className={classes.text}>
+                My Movies
+            </div>
+            <div 
+                ref={scrollDivRef}
+                className={classes.root}
+                onMouseDown={e => handleMouseDown(e)}
+                onMouseMove={e => handleMouseMove(e)}
+                onMouseUp={() => handleEndDrag()}
+                onMouseLeave={() => handleEndDrag()}
+            >
+                <div 
+                    className={classes.scrollBox}
+                    // Inline style to handle scroll amount (temporarily)
+                    style={{ right: scrollAmount }}
+                >
+                    {tileData.map((tile) => (
+                        <img className={classes.imgDimensions} src={tile.img} alt={tile.title} />
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 
