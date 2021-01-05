7387a971b46dac530cadc8b419511433b2acb16f
nglis <nglisic10@gmail.com>
2021-01-04 23:37:33 -0700

    server implementation

--- movi/src/App.js
+++ movi/src/App.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 import HomePage from './Containers/HomePage';
 
+import axios from 'axios';
+
 function App() {
+  const [test, setTest] = useState('hello');
+
+  /* useEffect(() => {
+    axios.get('/api/v1/say-something').then((res) => {
+      const response = res.data.body;
+      setTest(response);
+      console.log(res)
+    });
+  }, []); */
+
   return (
     <HomePage />
   );
