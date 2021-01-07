599e0100c635cbbf45a7fe1a0fd4b6fe7c18c63b
nglis <nglisic10@gmail.com>
2021-01-06 23:50:18 -0700

    Additional functions for handling movie data

--- movi/src/Other/MovieDataHandler.js
+++ movi/src/Other/MovieDataHandler.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 // Date is stored in YYYY-MM-DD format
 export const getYearFromDate = date => {
     if (!date || date === '') return '';
@@ -12,4 +14,39 @@ export const getYearFromDate = date => {
 export const removeTags = str => {
     if (!str || str === '') return '';
     return str.replace(/(<([^>]+)>)/gi, "");
+}
+
+export const getDataByGenre = (data, genre) => {
+    const items = [];
+    if (!data || _.isEmpty(data)) return items;
+
+    for (let item of data) {
+        const availableGenres = _.get(item, 'genres', []);
+        if (availableGenres.includes(genre)) {
+            items.push(item);
+        }
+    }
+
+    return items;
+}
+
+export const convertDataForScrollContainer = (data, limit) => {
+    const items = [];
+    if (!data || _.isEmpty(data)) return items;
+    for (let item of data) {
+        if (items.length === limit) break;
+        const img = _.get(item, 'image.medium', null);
+        const title = _.get(item, 'name', '');
+
+        if (!img || !title) continue;
+
+        items.push(
+            {
+                img,
+                title
+            }
+        );
+    }
+
+    return items;
 }
