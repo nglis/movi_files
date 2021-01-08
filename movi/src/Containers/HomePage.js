da4d0e958a584aa4bdfd4106c0c9e69aa31c4696
nglis <nglisic10@gmail.com>
2021-01-07 21:58:12 -0700

    Updated HomePage to handle data from website

--- movi/src/Containers/HomePage.js
+++ movi/src/Containers/HomePage.js
@@ -1,25 +1,50 @@
+import _ from 'lodash';
+
 import HeroDisplay from '../Components/HeroDisplay';
 import HorizontalScrollContainer from '../SubContainers/HorizontalScrollContainer';
 
-function HomePage() {
-    const details = { 
-      title: "TRAVELERS",
-      year: "2018",
-      rating: "TV-MA",
-      length: "3 Seasons",
-      episode: 'S3:E2 "Yates"',
-      description: "MacLaren and Yates adjust to their new partnership while protecting a TV show host whose inflammatory rhetoric about Travelers has led to tragedy"
+import { getDataByGenre, getYearFromDate, removeTags } from '../Other/MovieDataHandler';
+
+function HomePage(props) {
+    const { data } = props;
+
+    const firstEntry = !_.isNil(data) && !_.isEmpty(data) ? data[0] : {};
+
+    const title = _.get(firstEntry, 'name', 'Unknown Title');
+    const date = _.get(firstEntry, 'premiered', '');
+    const rating = _.get(firstEntry, 'rating.average', '');
+    const length = _.get(firstEntry, 'runtime', '');
+    const description = _.get(firstEntry, 'summary', '');
+
+    const heroData = { 
+      title,
+      rating,
+      length,
+      episode: '',
+      year: getYearFromDate(date),
+      description: removeTags(description)
     };
-  
+
     return(
         <div className="App">
           <header>
               MOVI
           </header>
           <HeroDisplay 
-            details={details}
+            details={heroData}
           />
-          <HorizontalScrollContainer />
+          <HorizontalScrollContainer 
+            collectionTitle="Drama"
+            content={getDataByGenre(data, "Drama")}
+          />
+          <HorizontalScrollContainer 
+            collectionTitle="Comedy"
+            content={getDataByGenre(data, "Comedy")}
+          />
+          <HorizontalScrollContainer 
+            collectionTitle="Thriller"
+            content={getDataByGenre(data, "Thriller")}
+          />
       </div>
     );
 }
