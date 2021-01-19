064b966e6b13f2e12208392453b6b59c17e3a63e
nglis <nglisic10@gmail.com>
2021-01-18 23:57:55 -0700

    Updated data req management

--- movi/src/Containers/HomePage.js
+++ movi/src/Containers/HomePage.js
@@ -1,4 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
 import _ from 'lodash';
+import axios from 'axios';
+
+import { CircularProgress } from '@material-ui/core';
 
 import AppBar from './AppBar';
 import HeroDisplay from '../Components/HeroDisplay';
@@ -7,14 +12,52 @@ import HorizontalScrollContainer from '../SubContainers/HorizontalScrollContaine
 import { getDataByGenre, getEntryFromCatalogByIndex, generatePreviewData } from '../Other/MovieDataHandler';
 
 function HomePage(props) {
-    const { data } = props;
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false);
 
     // TODO
-    const videoData = _.get(data, 'preview');
+    const videoData = _.get(data, 'preview', {});
     const heroPreviewData = generatePreviewData(videoData);
 
-    const catalog = _.get(data, 'catalog');
-    const heroCatalogData = getEntryFromCatalogByIndex(catalog, 0);
+    const catalog = _.get(data, 'catalog', []);
+    const catalogItemIndex = Math.floor(Math.random() * catalog.length);
+    const heroCatalogData = getEntryFromCatalogByIndex(catalog, catalogItemIndex);
+
+    useEffect(() => {
+      async function getData() {
+        let c = [];
+        let v = {};
+        setLoading(true);
+         await axios.get('http://api.tvmaze.com/shows').then((res) => {
+          if (res.statusText !== 'OK') return;
+          c = res.data;
+          //setData(data => ({...data, catalog: res.data}));
+        });
+  
+        await axios.get('https://www.googleapis.com/youtube/v3/search', 
+          {
+            params: {
+                q: 'Under the Dome',
+                part: 'snippet',
+                type: "video",
+                maxResults: 1,
+                key: process.env.REACT_APP_YOUTUBE_API_KEY
+            }
+          }).then((res) => {
+          if (res.status !== 200) return;
+          v = res.data;
+          //setData(data => ({...data, preview: res.data}))
+        }); 
+        setData( { catalog: c, preview: v } );
+        setLoading(false);
+      }
+  
+      getData();
+    }, []);
+
+    if (loading) {
+      return <CircularProgress />;
+    }
 
     return(
         <div className="App">
