a6da70fbed44f01960aece232ee1e0ef3e3d60f1
nglis <nglisic10@gmail.com>
2021-01-19 23:51:10 -0700

    Handling data changes

--- movi/src/Containers/HomePage.js
+++ movi/src/Containers/HomePage.js
@@ -11,49 +11,59 @@ import HorizontalScrollContainer from '../SubContainers/HorizontalScrollContaine
 
 import { getDataByGenre, getEntryFromCatalogByIndex, generatePreviewData } from '../Other/MovieDataHandler';
 
-function HomePage(props) {
-    const [data, setData] = useState({});
+function HomePage() {
+    const [catalog, setCatalogData] = useState({});
+    const [videoData, setVideoData] = useState({});
+
+    const [heroCatalogData, setHeroCatalogData] = useState();
+
     const [loading, setLoading] = useState(false);
 
     // TODO
-    const videoData = _.get(data, 'preview', {});
     const heroPreviewData = generatePreviewData(videoData);
 
-    const catalog = _.get(data, 'catalog', []);
-    const catalogItemIndex = Math.floor(Math.random() * catalog.length);
-    const heroCatalogData = getEntryFromCatalogByIndex(catalog, catalogItemIndex);
+    console.log(heroCatalogData)
 
     useEffect(() => {
       async function getData() {
         let c = [];
-        let v = {};
         setLoading(true);
          await axios.get('http://api.tvmaze.com/shows').then((res) => {
           if (res.statusText !== 'OK') return;
           c = res.data;
-          //setData(data => ({...data, catalog: res.data}));
         });
-  
-        await axios.get('https://www.googleapis.com/youtube/v3/search', 
-          {
-            params: {
-                q: 'Under the Dome',
-                part: 'snippet',
-                type: "video",
-                maxResults: 1,
-                key: process.env.REACT_APP_YOUTUBE_API_KEY
-            }
-          }).then((res) => {
-          if (res.status !== 200) return;
-          v = res.data;
-          //setData(data => ({...data, preview: res.data}))
-        }); 
-        setData( { catalog: c, preview: v } );
-        setLoading(false);
+        setCatalogData(c);
       }
   
       getData();
     }, []);
+
+    useEffect(() => {
+      const catalogItemIndex = Math.floor(Math.random() * catalog.length);
+      const newHeroCatalogData = getEntryFromCatalogByIndex(catalog, catalogItemIndex);
+      setHeroCatalogData(newHeroCatalogData);
+      setLoading(false);
+
+      async function getData() {
+        let v = {};
+        await axios.get('https://www.googleapis.com/youtube/v3/search', 
+        {
+          params: {
+              q: newHeroCatalogData.title,
+              part: 'snippet',
+              type: "video",
+              maxResults: 1,
+              key: process.env.REACT_APP_YOUTUBE_API_KEY
+          }
+        }).then((res) => {
+          if (res.status !== 200) return;
+          v = res.data;
+        }); 
+        setVideoData(v);
+      }
+
+      getData();
+    }, [catalog])
     
     return(
         <div className="App">
