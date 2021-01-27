9d119c0f43343cd432adb6e061b094e40e308984
nglis <nglisic10@gmail.com>
2021-01-26 23:47:12 -0700

    Updated app bar appearance

--- movi/src/Styles/Containers/AppBar.js
+++ movi/src/Styles/Containers/AppBar.js
@@ -2,7 +2,12 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
     root : {
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        textAlign: 'left',
+        fontSize: '70px',
+        fontWeight: '850',
+        color: 'silver',
+        marginLeft: '30px'
     },
   })
 );
