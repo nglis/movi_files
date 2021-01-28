b693121167f79d8077fcaeebbd1015489847198f
nglis <nglisic10@gmail.com>
2021-01-27 23:56:39 -0700

    Beginning implementation of popup info dialog

---
+++ movi/src/Components/PopupInfoDialog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { useStyles } from '../Styles/Containers/PopupInfoDialog.js';
+
+function PopupInfoDialog () {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            EMPTY
+        </div>
+    );
+}
+
+export default PopupInfoDialog;
