75ea6890f73117fd4fc2704c602b1e9c0094fe9b
nglis <nglisic10@gmail.com>
2021-02-04 23:00:30 -0700

    Added title and details to popup info dialog

--- movi/src/Components/PopupInfoDialog.js
+++ movi/src/Components/PopupInfoDialog.js
@@ -2,19 +2,29 @@ import React from 'react';
 
 import { useStyles } from '../Styles/Components/PopupInfoDialog';
 
+import { generateEntry } from '../Other/MovieDataHandler';
+
 function PopupInfoDialog (props) {
     const classes = useStyles();
 
-    const { data, setShowPopupInfoDialog } = props;
+    const { dialogData, setShowPopupInfoDialog } = props;
+    const data = generateEntry(dialogData);
+
+    console.log(data)
 
     return (
         <>
             <div 
                 className={classes.background}
                 onClick={() => setShowPopupInfoDialog(false)}
-                />
+            />
             <div className={classes.container}>
-                Test
+                <div className={classes.title}>
+                    {data.title}
+                </div>
+                <div className={classes.description}>
+                    {data.description}
+                </div>
             </div>
         </>
     );
--- movi/src/Styles/Components/PopupInfoDialog.js
+++ movi/src/Styles/Components/PopupInfoDialog.js
@@ -11,15 +11,35 @@ export const useStyles = makeStyles((theme) => ({
       left: 0, 
       position: 'fixed'
     },
+
     container: {
-      width: '200px',
-      height: '200px',
+      width: '25em',
+      height: '15em',
       position: 'absolute',
+      background: '#1c1c1c', 
       top: '50%',
       left: '50%',
-      marginTop: '-100px',
-      marginLeft: '-100px',
-      zIndex: 10
+      margin: 0,
+      zIndex: 10,
+      transform: 'translate(-50%, -50%)',
+      border: 'none',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      borderRadius: '10px'
+    },
+    
+    title: {
+      fontSize: 'calc(20px + 4vmin)',
+      fontWeight: '600',
+      marginBottom: '20px'
+    },
+
+    description: {
+      fontSize: 'calc(10px + 1vmin)',
+      margin: '5px',
+      marginTop: 0,
+      textAlign: 'center'
     }
   }
   ));
