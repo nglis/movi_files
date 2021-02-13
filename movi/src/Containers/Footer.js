9c9a0a1a3eaf47944f901d83f44f34e996df864b
nglis <nglisic10@gmail.com>
2021-02-12 22:48:38 -0700

    Added share buttons

--- movi/src/Containers/Footer.js
+++ movi/src/Containers/Footer.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import { useStyles } from '../Styles/Containers/Footer.js';
 
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+
 function Footer () {
     const classes = useStyles();
 
     return (
         <>
         <div className={classes.footerShare}>
-            <div classname={classes.footerShareButtons}>
-                Share buttons will go here
+            <div className={classes.footerShareButtons}>
+                <InstagramIcon className={classes.shareButton}/>
+                <FacebookIcon className={classes.shareButton}/>
+                <TwitterIcon className={classes.shareButton}/>
+                <WhatsAppIcon className={classes.shareButton}/>
             </div>
             <div className={classes.footerShareNavigation}>
                 Navigation links will go here
