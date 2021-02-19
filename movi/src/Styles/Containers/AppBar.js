c772d3d3d0c811763a4b3b5b9c3c4a26acf61a4c
nglis <nglisic10@gmail.com>
2021-02-18 23:28:43 -0700

    Added styling to NavBar

--- movi/src/Styles/Containers/AppBar.js
+++ movi/src/Styles/Containers/AppBar.js
@@ -3,11 +3,23 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
     root : {
         backgroundColor: 'black',
-        textAlign: 'left',
         fontSize: '70px',
         fontWeight: '850',
         color: 'silver',
-        paddingLeft: '30px'
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
+
+    title: {
+      marginLeft: '30px'
+    },
+
+    navButtons: {
+      marginRight: '30px',
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: '60px'
+    }
   })
 );
